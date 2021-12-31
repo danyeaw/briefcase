0a46c3857535d18eba48594b4410cee3060ed666
Mansour Moufid <mansourmoufid@gmail.com>
2021-12-31 14:39:03 -0500

    Install packages into an ABI-specific subdirectory of app_packages.

--- src/briefcase/commands/create.py
+++ src/briefcase/commands/create.py
@@ -385,10 +385,9 @@ class CreateCommand(BaseCommand):
         Return a tuple of the Python shared library name and its path.
         """
         if self.platform == "android":
+            abi = getattr(app, "ABI", "arm64-v8a")
             # sysconfig.get_config_var("LIBDIR")
-            libdir = os.path.join(
-                self.bundle_path(app), "app", "libs", "arm64-v8a"
-            )
+            libdir = os.path.join(self.bundle_path(app), "app", "libs", abi)
             if not os.path.exists(libdir):
                 return (None, None)
             # sysconfig.get_config_var("LDLIBRARY")
@@ -412,7 +411,8 @@ class CreateCommand(BaseCommand):
         :param app: The config object for the app
         """
         if self.platform == "android":
-            env = self.android_sdk.env
+            env = dict(self.android_sdk.env)
+            env.update({"ABI": getattr(app, "ABI", "arm64-v8a")})
         else:
             env = dict(os.environ)
         includepy = self.include_path(app)
@@ -437,7 +437,6 @@ class CreateCommand(BaseCommand):
 
         :param app: The config object for the app
         """
-
         target = self.app_packages_path(app)
 
         # Clear existing dependency directory
@@ -445,6 +444,16 @@ class CreateCommand(BaseCommand):
             self.shutil.rmtree(target)
             self.os.mkdir(target)
 
+        if self.platform == "android":
+            abi = getattr(app, "ABI", None)
+            if abi is None:
+                for abi in ["armeabi-v7a", "arm64-v8a", "x86", "x86_64"]:
+                    app.ABI = abi
+                    self.install_app_dependencies(app)
+                del app.ABI
+                return
+            else:
+                target = os.path.join(target, abi)
         # Install  dependencies
         if app.requires:
             try:
